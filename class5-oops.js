532368affbf32edde9a9225430ed3c08f36e0498
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2021-07-08 22:00:41 +0530

    class 6 done

--- class5-oops.js
+++ class5-oops.js
@@ -92,20 +92,154 @@ function createElf1(name, weapon) {
         weapon,
     }
 }
-const peter = createElf1('peter', 'stones');
-peter.attack = elfFunctions.attack;
-console.log(peter.attack());
+// const peter = createElf1('peter', 'stones');
+// peter.attack = elfFunctions.attack;
+// console.log(peter.attack());
 
-// prototype
+// Object.Create()
+function createElf2(name, weapon) {
+    // O
+    let newElf = Object.create(elfFunctions);
+    newElf.name = name;
+    newElf.attack = elfFunctions.attack;
+    newElf.weapon = weapon;
+    return  newElf;
+}
+// const peter2 = createElf2('peter', 'stones');
+// console.log(peter2.attack());
 
-//  this => function ()=>
+// constructor functions
+// close to oops
+function ElfConstruct(name, weapon) {
+   this.name =  name;
+   this.weapon = weapon;
+}
+// ElfConstruct.prototype.attack = function (){
+//     return  'attack with ' + this.weapon; // this  => peter3
+// }
+ElfConstruct.prototype.attack =  () =>{
+    return  'attack with ' + this.weapon; // this => global this =  windows
+    // () => lexically scoped => this =>
+    // function => dynamically scoped => this => based on who call it
+}
+// const peter3 = new ElfConstruct('Peter','stones'); //
+// console.log(peter3.attack());
 
-// new kw
-// funny js
-// class
-// inheritance
-// extends
-// polymorphism
-// encapsulation
-// abstraction
+//
+let a = 5;
+let b = new Number(5);
+// console.log(a);
+// console.log(b);
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(a == b );
+// console.log(a === b);
 
+// ===
+// new
+
+
+// using class
+class Elf {
+    constructor(name, weapon) {
+        this.name = name; // this =  peter4
+        this.weapon = weapon;
+    }
+    attack() {
+        return 'attack with ' + this.weapon;
+    }
+    //
+    //
+}
+const peter4 = new Elf('Peter','bow'); //
+//console.log(peter4.attack());
+
+// this keyword
+// new binding
+function  Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+const person1 = new Person('Xavier', 55);
+
+// implicit binding
+const personImplicit = {
+    name : 'Karen',
+    age : 120,
+    hi() {
+        console.log('hi' + this.name);
+    }
+}
+
+//personImplicit.hi();
+
+// Inheritance
+class Elf1 {
+    constructor(name, weapon)  {
+        this.name = name;
+        this.weapon = weapon;
+    }
+    attack() {
+        return 'attack with ' + this.weapon;
+    }
+}
+// const fiona = new Elf('Fioan', 'ninja');
+// const ogre = {...fiona};
+// console.log(fiona.attack());
+// //console.log(ogre.attack());
+// console.log(ogre === fiona);
+
+class Character {
+    constructor(name, weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+    attack() {
+        return 'attack with ' + this.weapon;
+    }
+}
+
+class ElfInherit extends Character {
+    constructor(name, weapon, type) {
+        super(name,weapon);
+        this.type = type;
+    }
+    // sing
+    // call
+}
+
+class Orge1 extends Character {
+    constructor(name, weapon,color) {
+        super(name, weapon);
+        this.color = color;
+    }
+    makeFort() {
+        return 'Strongest fort in the world';
+    }
+}
+const dolby = new ElfInherit('Dolby', 'cloth' ,'house');
+console.log(dolby.attack());
+const shrek = new Orge1('Shrek', 'club', 'green');
+console.log(shrek.attack());
+console.log(shrek.makeFort());
+
+
+// Exercise
+/*
+class Character {
+    constructor(name, weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+
+    attack() {
+        return 'attack with ' + this.weapon
+    }
+}
+*/
+
+//Polymorphism--
+//Extend the Character class to have a Queen class. The output of the below code should be:
+//const victoria = new Queen('Victoria', 'army', 'hearts'); // create a new instance with the queen having (name, weapon, type). Type includes: 'hearts', 'clubs', 'spades', 'diamonds'
+
+//victoria.attack() // will console.log the attack() method in Character class AND will return another string: 'I am the Victoria of hearts, now bow down to me! '
