e24d5096fc4974b3b16460e88cec12f7adaab263
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2021-07-20 08:58:53 +0530

    some problems

--- dummy.js
+++ dummy.js
@@ -1,3 +1,4 @@
+/*
 const array = [
     [123, 2342, 34, 234, 234, 32],
     [123, 234, 234, 32],
@@ -10,4 +11,23 @@ const array = [
 
 console.log(array.sort((a, b) => {
     return a.sort((a, b) => a - b).length - b.sort((a, b) => a - b).length;
-}));
+}));*/
+
+const solution = (S) => {
+    let front = 0;
+    let back = 0;
+    let op = '';
+    while (front < S.length) {
+        if (S[front] === 's')
+            if (S[back] === 'm') {
+                op += '-';
+                back = front + 1;
+            } else if (S[back] === 'p') {
+                op += '+';
+                back = front + 1;
+            }
+        front++;
+    }
+    return op;
+}
+console.log(solution("plusminusminusplusminusplusplusplusminus"));
