cdd907cc908e6a39897f062bf4d10ff4de07d190
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2021-07-20 11:09:17 +0530

    donegg

--- dummy.js
+++ dummy.js
@@ -31,3 +31,96 @@ const solution = (S) => {
     return op;
 }
 console.log(solution("plusminusminusplusminusplusplusplusminus"));
+
+
+// Input -  activities and tasks
+// Expected output - Activity-1 has completed 0%
+// Activity-2 has completed 100%..... and so on
+const activities = [
+    {
+        name: 'activity-1',
+        id: 'ACT-1',
+        tasks: [1, 2]
+    },
+    {
+        name: 'activity-2',
+        id: 'ACT-2',
+        tasks: [3]
+    },
+    {
+        name: 'activity-3',
+        id: 'ACT-3',
+        tasks: [4, 5, 6, 7, 8]
+    },
+    {
+        name: 'activity-4',
+        id: 'ACT-4',
+        tasks: [9, 10]
+    },
+]
+// statuses
+// 0 - Not Started
+// 1 - Work in progress
+// 2 - Completed
+const tasks = [
+    {
+        name: 'Task-1',
+        id: 1,
+        status: 0,
+        activity: "ACT-1"
+    },
+    {
+        name: 'Task-2',
+        id: 2,
+        status: 0,
+        activity: "ACT-1"
+    },
+    {
+        name: 'Task-3',
+        id: 3,
+        status: 2,
+        activity: "ACT-2"
+    },
+    {
+        name: 'Task-4',
+        id: 4,
+        status: 2,
+        activity: "ACT-3"
+    },
+    {
+        name: 'Task-5',
+        id: 5,
+        status: 2,
+        activity: "ACT-3"
+    },
+    {
+        name: 'Task-6',
+        id: 6,
+        status: 0,
+        activity: "ACT-3"
+    },
+    {
+        name: 'Task-7',
+        id: 7,
+        status: 1,
+        activity: "ACT-3"
+    },
+    {
+        name: 'Task-8',
+        id: 8,
+        status: 2,
+        activity: "ACT-3"
+    },
+    {
+        name: 'Task-9',
+        id: 9,
+        status: 2,
+        activity: "ACT-4"
+    },
+    {
+        name: 'Task-10',
+        id: 10,
+        status: 2,
+        activity: "ACT-4"
+    }
+]
